ed9af23ad0621e9f2633276ef3f95db0013fa970
Ashish Chaturvedi <ashish210200@gmail.com>
2025-07-14 00:46:16 +0530

    PUSH AICHATBOT CODE

--- Applications/AIChatbot/main.py
+++ Applications/AIChatbot/main.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-key = "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"
+key = "" #use you OpenAI API Key
 
 messages = []
 
