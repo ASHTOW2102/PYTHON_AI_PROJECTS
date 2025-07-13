526719eba18126e5201b18b453b16c08dd562a7b
Ashish Chaturvedi <ashish210200@gmail.com>
2025-07-14 00:36:35 +0530

    AIBot code push

---
+++ Applications/AIChatbot/main.py
@@ -0,0 +1,36 @@
+from openai import OpenAI
+key = "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"
+
+messages = []
+
+client = OpenAI(
+    api_key=key,  # This is the default and can be omitted
+)
+
+def completion(message):
+    global messages
+    messages.append(
+        {
+            "role": "user",
+            "content": message
+        }
+    )
+
+    chat_completion = client.chat.completions.create( messages=messages,
+                        model="gpt-4o"
+                        )
+    
+    # print(chat_completion)
+    message = {
+        "role": "assistant",
+        "content": chat_completion.choices[0].message.content
+    }
+    messages.append(message)
+    print(f"\nAIBot: {message["content"]}\n")
+    
+
+
+while True:
+        print(f"AIBot: Hi I am AIBot, How may I help you\n")
+        user_question = input("User: ")
+        completion(user_question)
